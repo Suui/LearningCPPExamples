a6c2bbbd51872912504b58c6579a2d4967e26c36
Suui <suui.gd@gmail.com>
2015-05-18 16:24:01 +0200

    NewKeywordExample finished

--- Dynamic Memory Allocation/NewKeywordExample.cpp
+++ Dynamic Memory Allocation/NewKeywordExample.cpp
@@ -20,9 +20,9 @@ void NewKeywordExample::execute()
 	// don't need to specify it. However, most of the time I would note use it if it's not easily readable.
 
 	auto Angelo = Player("Angelo");			// Normal Player03 constructed, not a pointer, not dynamically allocated.
-	auto Jose = new Player("Jose");		// Player02 constructed
+	auto Jose = new Player("Jose");			// Player02 constructed
 
 	delete Jose;	// Without this delete, the Player03 object would stay in allocated in memory
-	// even after our program ends. It won't be destroyed when out of scope!
+					// even after our program ends. It won't be destroyed when out of scope!
 	std::cout << "==> New Keyword Example finished!" << std::endl;
 }
